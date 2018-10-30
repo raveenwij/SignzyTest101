fe8dab53fc87dca6d8632222d410a0411b75d9b4
raveenwij <ravwij@gmail.com>
2018-10-30 19:22:02 +0530

    accesstoken

--- SignzyTest101/lambda.js
+++ SignzyTest101/lambda.js
@@ -12,7 +12,24 @@ exports.handler = function (event, context, callback) {
         }
     }).then((response) => {
         // your code goes here
-        console.log{response};
+        console.log(response);
+
+        Swagger.http({
+            url: `https://api.dev.fintechmart.online/signzynew/2.0/api/v2/patrons/58834495336daf647411278e}/organizations`,
+            method: 'post',
+            query: {},
+            headers: { "X-Authorization": response.access_token, "Accept": "application/json", "Content-Type": "application/json" },
+            body: {
+                "identifier": "U74900MH2011PTC216800",
+                "service": "roc",
+                "callbackUrl": "https://signzy.com"
+            }
+        }).then((response) => {
+            // your code goes here
+        }).catch((err) => {
+            // error handling goes here
+        });
+
     }).catch((err) => {
         // error handling goes here
     });
